6c65819f52e6acafdabb10fe656dc8db8d00f53b
EricJawB <32011822+EricJawB@users.noreply.github.com>
2026-02-12 09:23:04 +0300

    Initial Commit

---
+++ gradlew.bat
@@ -0,0 +1,16 @@
+\
+@rem Gradle startup script for Windows
+@echo off
+setlocal
+
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+if defined JAVA_HOME (
+  set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+) else (
+  set JAVA_CMD=java.exe
+)
+
+"%JAVA_CMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+endlocal
